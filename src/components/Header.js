54d00b94cc487839b11d9e2ddb48ae309869ad09
LouisDigit <louisv.digit@gmail.com>
2022-04-27 08:58:02 +0200

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>My Todo List</h1>
+      <i class="fa-solid fa-list-check"></i>
+    </header>
+  );
+};
+export default Header;
